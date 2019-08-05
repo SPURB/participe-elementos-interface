3f9ec74474bd0460e37339ce74336b90d6c18738
davimh <hosogiri.davi@gmail.com>
2019-08-05 11:29:35 -0300

    Adiciona imagens de consultas faltantes

--- tarefas/capas.js
+++ tarefas/capas.js
@@ -5,32 +5,61 @@ const file = process.env.npm_package_config_arquivo
 
 const inputFolder = './capas/'
 const outputFolder = './publicos/capas/'
-const sizes = [ 1900, 1300, 1000, 700, 480, 15 ]
+// consultas abertas: 800x450 (1x) / 1600x900 (2x)
+// consultas encerradas: 122x68 (1x) / 244x136 (2x)
+// placeholder: 40x40
+const sizes = [
+    { 'width': 1600, 'height': 900, 'for': 1 }, // abertas, 2x
+    { 'width': 800, 'height': 450, 'for': 1 },  // abertas, 1x
+    { 'width': 244, 'height': 136, 'for': 2 },  // encerradas, 2x
+    { 'width': 122, 'height': 68, 'for': 2 },   // encerradas, 1x
+    { 'width': 40, 'height': 40, 'for': 3 }     // placeholder
+]
 
 let setup = dir => { !fs.existsSync(dir) && fs.mkdirSync(dir) }
 setup(outputFolder)
 
 let inputPath = file => `${ inputFolder }${ file }`
-let createFile = ( input, width ) => {
+let createFile = ( input, size ) => {
     const file = input.replace(inputFolder, '')
 
     const extension = file.split('.').pop()
     const fileNoExtension = file.replace('.' + extension, '')
 
-    sharp(input)
-        .resize({ width: width })
-        .toFile(`${outputFolder}${fileNoExtension}_${width}w.${extension}`)
-        .catch( err => {
-            throw new Error(err)
-        })
+    if (size.for != 3) {
+        sharp(input)
+            .resize({ width: size.width, height: size.height, fit: sharp.fit.cover })
+            .toFile(`${outputFolder}${fileNoExtension}_${size.width}w.${extension}`)
+            .catch( err => {
+                throw new Error(err)
+            })
 
-    sharp(input)
-        .resize({ width: width })
-        .webp()
-        .toFile(`${outputFolder}${fileNoExtension}_${width}w.webp`)
-        .catch( err => {
-            throw new Error(err)
-        })
+        sharp(input)
+            .resize({ width: size.width, height: size.height, fit: sharp.fit.cover })
+            .webp()
+            .toFile(`${outputFolder}${fileNoExtension}_${size.width}w.webp`)
+            .catch( err => {
+                throw new Error(err)
+            })
+    }
+    else if (size.for == 3) {
+        sharp(input)
+            .resize({ width: size.width, height: size.height, fit: sharp.fit.cover })
+            .blur(5)
+            .toFile(`${outputFolder}${fileNoExtension}_${size.width}w.${extension}`)
+            .catch( err => {
+                throw new Error(err)
+            })
+
+        sharp(input)
+            .resize({ width: size.width, height: size.height, fit: sharp.fit.cover })
+            .blur(5)
+            .webp()
+            .toFile(`${outputFolder}${fileNoExtension}_${size.width}w.webp`)
+            .catch( err => {
+                throw new Error(err)
+            })
+    }
 }
 
 if (file === 'default'){
